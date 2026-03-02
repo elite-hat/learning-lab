84b13034757e63adc79fb9f0c1f1861b59a2ea66
elite-hat <elitehat.developer@gmail.com>
2026-03-02 21:00:18 +0500

    *

---
+++ python/crash-course/tutorials/tutorial-5.py
@@ -0,0 +1,27 @@
+# Strings
+
+course = "Python's course for beginners"
+print(course)
+
+course = 'Python for "Beginners"'
+print(course)
+
+course = '''
+
+Hi John,
+
+Here is our first email to you.
+
+Thank You,
+The support team
+
+'''
+
+course = "Python for Beginners"
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[1:])
+print(course[:5])
+print(course[:])
