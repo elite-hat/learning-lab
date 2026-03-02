a7e516f44c6acbef8fb85a2950b33a2926b3c608
elite-hat <elitehat.developer@gmail.com>
2026-03-02 14:35:25 +0500

    *

---
+++ python/crash-course/exercises/exercise-2.py
@@ -0,0 +1,11 @@
+# Ask two questions: person's name and favorite color. Then print a message like "Mosh likes blue".
+
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+print(name + " likes " + color)
+
+# Solution
+
+#name = input("What is your name? ")
+#favorite_color = input("What is your favorite color? ")
+#print(name + ' likes ' + favorite_color)
