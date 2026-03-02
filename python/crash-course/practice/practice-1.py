7f454ca50112620daca568778225c93a3d8c4b07
elite-hat <elitehat.developer@gmail.com>
2026-03-02 07:21:47 +0500

    Py/CC/P Practice 1

---
+++ python/crash-course/practice/practice-1.py
@@ -0,0 +1,11 @@
+# We check in a patient named John Smith. He's 20 years old is a new patient.
+
+name = "John Smith"
+age = 20
+isNew = True
+
+# Solution
+
+# full_name = "John Smith"
+# age = 20
+# is_new = True
