6c96e1b80acb2e620eaa93408dc5a0866ab00085
elite-hat <elitehat.developer@gmail.com>
2026-03-02 21:21:10 +0500

    *

---
+++ python/crash-course/tutorials/tutorial-7.py
@@ -0,0 +1,27 @@
+# String Methods
+
+course = "Python for Beginners"
+
+print(len(course))
+print(course)
+print(course.upper())
+print(course.lower())
+
+print(course.find("P"))
+print(course.find("o"))
+print(course.find("Beginners"))
+
+print(course.replace("Beginners", "Absolute Beginners"))
+print(course.replace("P", "J"))
+
+print("Python" in course)
+
+# len()
+# upper()
+# lower()
+# title
+# find()
+# replace()
+# '...' in variable
+
+print(course.title())
