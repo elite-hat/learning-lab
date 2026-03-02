7553130988620dd979528ca68b6785b8b94039bc
elite-hat <elitehat.developer@gmail.com>
2026-03-02 21:27:15 +0500

    *

---
+++ python/crash-course/tutorials/tutorial-8.py
@@ -0,0 +1,17 @@
+# ARITHMETIC OPERATORS
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+# increment
+
+x = 10
+x += 3
+print(x)
+x -= 3
+print(x)
