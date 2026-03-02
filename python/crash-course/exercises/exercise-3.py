2782c9110e4b6bfdb302776aaeb592dda3a2d86a
elite-hat <elitehat.developer@gmail.com>
2026-03-02 14:54:00 +0500

    *

---
+++ python/crash-course/exercises/exercise-3.py
@@ -0,0 +1,11 @@
+# Ask a user there weight (in pounds), convert into kilograms and print on the terminal.
+
+pounds = input("Enter your weight in pounds: ")
+kg = 0.454 * float(pounds)
+print("Your weight in kilograms is: " + str(kg))
+
+# SOLUTION
+
+# weight_lbs = input("Weight (lbs):")
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
