bf4deb22ecda2125c6f6e2cd18aaa1bd79d5bdf9
elite-hat <elitehat.developer@gmail.com>
2026-03-03 21:30:40 +0500

    #

---
+++ python/crash-course/tutorials/tutorial-10.py
@@ -0,0 +1,14 @@
+# IF STATEMENTS
+
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It is a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It is a cold day")
+    print("You should wear warm clothes")
+else:
+    print("It is a lovely day")
+print("Enjoy your day")
