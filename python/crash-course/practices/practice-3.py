91af7f89bdb954b4867785ab18a7eb3d423a2b50
elite-hat <elitehat.developer@gmail.com>
2026-03-03 18:00:26 +0500

    *

--- python/crash-course/practices/practice-3.py
+++ python/crash-course/practices/practice-3.py
@@ -5,15 +5,6 @@ age = input("Age: ")
 height = input("Height: ")
 male = input("Are you male? ")
 
-age = int(age)
-height = float(height)
-male = bool(male)
-
-print(type(name))
-print(type(age))
-print(type(height))
-print(type(male))
-
 print(name)
 print(age)
 print(height)
