cd4e40edee7d683eeafe536b618b63e2b6b083ba
elite-hat <elitehat.developer@gmail.com>
2026-03-03 21:47:15 +0500

    #

--- python/crash-course/exercises/exercise-6.py
+++ python/crash-course/exercises/exercise-6.py
@@ -3,3 +3,20 @@
 # Otherwise they need to put down 20%
 
 # print the down payment
+
+housePrice = 1000000
+goodCredit = True
+if goodCredit:
+    print(housePrice * (10/100))
+else:
+    print(housePrice * (20/100))
+
+# SOLUTION
+
+price = 1000000
+has_good_credit = True
+if has_good_credit:
+    down_payment = price * 0.1
+else:
+    down_payment = price * 0.2
+print(f"Down Payment: ${down_payment}")
