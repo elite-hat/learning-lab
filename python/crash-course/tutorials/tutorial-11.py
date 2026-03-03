cbcb1dd6766b6df4b942f3e0a1fc46e00ad22822
elite-hat <elitehat.developer@gmail.com>
2026-03-03 21:55:26 +0500

    #

---
+++ python/crash-course/tutorials/tutorial-11.py
@@ -0,0 +1,11 @@
+# LOGICAL OPERATORS
+
+has_high_income = True
+has_good_credit = True
+has_criminal_record = False
+
+if has_high_income and has_good_credit:
+    print("Eligible for Loan")
+
+if has_good_credit and not has_has_criminal_record:
+    print("Eligible for Loan")
