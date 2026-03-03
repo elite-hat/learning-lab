59b5a567f590c6f6b1c53074cfaccbef94b1d2df
elite-hat <elitehat.developer@gmail.com>
2026-03-03 17:47:04 +0500

    *

---
+++ python/crash-course/practices/practice-1.py
@@ -0,0 +1,17 @@
+# Name
+
+print("Abdun Nafay Rathore")
+
+# Heart
+
+print("  *    *  ")
+print(" *  * *  * ")
+print(" *   *   * ")
+print("  *     *  ")
+print("   *   *   ")
+print("    * *    ")
+print("     *     ")
+
+# String Product
+
+print("Hello " * 10)
