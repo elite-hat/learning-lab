1cf1a39c9471bd54d0feb1f1725204688a4edf3a
elite-hat <elitehat.developer@gmail.com>
2026-03-04 21:10:22 +0500

    #

--- python/crash-course/projects/Project Solutions/solution-2.py
+++ python/crash-course/projects/Project Solutions/solution-2.py
@@ -1,10 +1,19 @@
 command = ""
+started = False
 while True:
     command = input("> ").lower()
     if command == "start":
-        print("Car Started...")
+        if started:
+            print("Car is already started.")
+        else:
+            started = True
+            print("Car started...")
     elif command == "stop":
-        print("Car stopped.")
+        if not started:
+            print("Car is already stopped.")
+        else:
+            started = False
+            print("Car stopped.")
     elif command == "help":
         print("""
 start - to start the car
