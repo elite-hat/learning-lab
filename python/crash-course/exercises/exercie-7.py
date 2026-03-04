38966d2bd389fae1198fb547f79afd5f02ebb778
elite-hat <elitehat.developer@gmail.com>
2026-03-04 21:28:01 +0500

    #

---
+++ python/crash-course/exercises/exercie-7.py
@@ -0,0 +1,14 @@
+prices = [10, 20, 30]
+result = 0
+for item in prices:
+    result += item
+
+print(result)
+
+# Solution
+
+# prices = [10, 20, 30]
+# total = 0
+# for price in prices:
+#    total += price
+# print(f"Total: {total})
