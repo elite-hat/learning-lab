142e871d61c7e42f3712f79b75df32a962f5e649
elite-hat <elitehat.developer@gmail.com>
2026-03-04 21:05:54 +0500

    #

--- python/crash-course/projects/project-2.py
+++ python/crash-course/projects/project-2.py
@@ -2,6 +2,7 @@
 
 cmd = input(">")
 i = 0
+start = False
 while i == 0:
     if cmd == "help":
         print("start - to start the car")
@@ -9,10 +10,18 @@ while i == 0:
         print("quit - to exit")
         cmd = input(">")
     elif cmd == "start":
-        print("Car started...Ready to go!")
+        if start == False:
+            start = True
+            print("Car started...Ready to go!")
+        else:
+            print("Car is already started.")
         cmd = input(">")
     elif cmd == "stop":
-        print("Car stopped.")
+        if start == True:
+            start = False
+            print("Car stopped.")
+        else:
+            print("Car is already stopped.")
         cmd = input(">")
     elif cmd == "quit":
         break
