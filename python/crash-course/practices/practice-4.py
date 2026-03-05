8648668f2caf5ae7f1daa30e804dbb741a1f4e24
elite-hat <elitehat.developer@gmail.com>
2026-03-05 15:13:12 +0500

    #

--- python/crash-course/practices/practice-4.py
+++ python/crash-course/practices/practice-4.py
@@ -1,4 +1,8 @@
 # TYPE CONVERSION
 
-obtainedMarks = input("Obtained Mark: ")
-totalMarks = input("Total Marks: ")
+totalMarks = int(input("Total Marks: "))
+obtainedMarks = int(input("Obtained Marks: "))
+deductedMarks = totalMarks - obtainedMarks
+percentage = int(obtainedMarks / totalMarks * 100)
+print(f"Deducted Marks: {deductedMarks}")
+print(f"Percentage: {percentage}%")
