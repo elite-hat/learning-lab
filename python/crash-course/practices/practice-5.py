4a4edbc6f8ea97720d445b12fa3f787000ada04b
elite-hat <elitehat.developer@gmail.com>
2026-03-05 15:21:07 +0500

    #

---
+++ python/crash-course/practices/practice-5.py
@@ -0,0 +1,19 @@
+# Strings
+
+name = "Abdun Nafay Rathore"
+
+print(name)
+print('Abdun "Nafay" Rathore')
+print("Abdun Nafay Rathore's Laptop")
+print(name[:])
+print(name[0])
+print(name[6])
+print(name[12])
+print(name[:5])
+print(name[12:])
+print(name[6:11])
+print('''
+Abdun
+Nafay
+Rathore
+''')
