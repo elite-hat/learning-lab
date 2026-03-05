3b3a033808c5522d564b83292f5b98198073d7e1
elite-hat <elitehat.developer@gmail.com>
2026-03-05 16:06:04 +0500

    #

--- python/crash-course/exercises/exercise-9.py
+++ python/crash-course/exercises/exercise-9.py
@@ -1,4 +1,17 @@
+# F
+
 numbers = [5, 2, 5, 2, 2]
+for x in numbers:
+    output = ""
+    for y in range(x):
+        output += "x"
+    print(output)
+
+print("")
+
+# L
+
+numbers = [2, 2, 2, 2, 5]
 for x in numbers:
     output = ""
     for y in range(x):
