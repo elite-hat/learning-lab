d8969a3ee97f11d9d5db1d273a39bbbdffc1c803
elite-hat <elitehat.developer@gmail.com>
2026-03-05 16:02:38 +0500

    #

---
+++ python/crash-course/exercises/exercise-9.py
@@ -0,0 +1,15 @@
+numbers = [5, 2, 5, 2, 2]
+for x in numbers:
+    output = ""
+    for y in range(x):
+        output += "x"
+    print(output)
+
+# SOLUTION
+
+# numbers = [5, 2, 5, 2, 2]
+# for x_count in numbers:
+#     output = ""
+#     for count in range(x_count):
+#         output += "x"
+#     print(output)
