613a60405316886168ebcc5a0e0eb28f9916e70c
elite-hat <elitehat.developer@gmail.com>
2026-03-05 15:24:53 +0500

    #

---
+++ python/crash-course/practices/practice-6.py
@@ -0,0 +1,7 @@
+firstName = "Abdun"
+middleName = "Nafay"
+lastName = "Rathore"
+print(firstName + " " + middleName + " " + lastName)
+print(f"{firstName} {middleName} {lastName}")
+name = f"{firstName} {middleName} {lastName}"
+print(name)
