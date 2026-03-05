381d60072fa7723e786f7bafa5e1323715622d0a
elite-hat <elitehat.developer@gmail.com>
2026-03-05 16:11:05 +0500

    #

---
+++ python/crash-course/tutorials/tutorial-17.py
@@ -0,0 +1,14 @@
+# LISTS
+
+names = ["John", "Bob", "Mosh", "Sarah", "Mary"]
+print(names)
+print(names[0])
+print(names[2])
+print(names[-1])
+print(names[-2])
+print(names[2:4])
+print(names[:])
+
+# modification
+
+names[0] = "Jon"
