3a38823a44e76ed978ff2276022339b02471e206
elite-hat <elitehat.developer@gmail.com>
2026-03-05 15:33:14 +0500

    #

---
+++ python/crash-course/practices/practice-7.py
@@ -0,0 +1,13 @@
+name = "Abdun Nafay Rathore"
+
+print(name)
+print(name.lower())
+print(name.upper())
+print(len(name))
+print(name.find("A"))
+print(name.find("e"))
+print(name.find("Rathore"))
+print("Nafay" in name) # True or False
+place = "Kotli"
+print(place.replace("Kotli", "Mirpur"))
+print(place.replace("t", "h"))
