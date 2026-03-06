d927b4edef428f439fb796574e60f536ad2c032c
elite-hat <elitehat.developer@gmail.com>
2026-03-06 14:42:25 +0500

    #

--- python/crash-course/tutorials/tutorial-22.py
+++ python/crash-course/tutorials/tutorial-22.py
@@ -1,1 +1,13 @@
 # DICTIONARIES
+
+customer = {
+    "name": "John Smith",
+    "age": 30,
+    "is_verified": True
+}
+print(customer["name"])
+print(customer.get("birthdate"))    # It doesn't exist in dictionary, but using .get allows us to use it wihtout giving an error.
+print(customer.get("birthdate", "Jan 1, 1980"))
+customer["name"] = "Jack Smith"
+print(customer["name"])
+customer["birthdate"] = "Jan 1 1980"
