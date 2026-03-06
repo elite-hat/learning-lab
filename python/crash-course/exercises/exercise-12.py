357bda191d98f805e5b3049ceebc0fad7e32384b
elite-hat <elitehat.developer@gmail.com>
2026-03-06 15:05:10 +0500

    #

---
+++ python/crash-course/exercises/exercise-12.py
@@ -0,0 +1,18 @@
+phone = input("Phone: ")
+digitDictionary = {
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine"
+}
+output = ""
+for digits in phone:
+    output += digitDictionary[digits] + " "
+print(output)
+
