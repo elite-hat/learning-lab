b062d2c8885a05df99c897a8be3ae527952e6eed
elite-hat <elitehat.developer@gmail.com>
2026-03-15 20:43:37 +0500

    #

--- python/crash-course/tutorials/tutorial-26.py
+++ python/crash-course/tutorials/tutorial-26.py
@@ -1,1 +1,12 @@
 # Keyword Arguments
+
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+print("Start")
+greet_user(last_name = "Smith", first_name = "John")
+print("Finish")
+
+# prefer positional arguments
+# if both types of arguments, forst positional
