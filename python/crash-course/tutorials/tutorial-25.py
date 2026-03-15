fcdc0078694a37eed780ac5f633f7b7f9380b504
elite-hat <elitehat.developer@gmail.com>
2026-03-15 20:34:50 +0500

    #

--- python/crash-course/tutorials/tutorial-25.py
+++ python/crash-course/tutorials/tutorial-25.py
@@ -1,1 +1,9 @@
 # Parameters
+
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+print("Start")
+greet_user("John", "Smith")
+print("Finish")
