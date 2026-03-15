3845814cb940fead67d8ce92141a2258baec648e
elite-hat <elitehat.developer@gmail.com>
2026-03-15 20:51:35 +0500

    #

---
+++ python/crash-course/tutorials/tutorial-27.py
@@ -0,0 +1,12 @@
+# Return Statement
+
+def square(number):
+    return number*number
+
+result = square(3)
+print(result)
+
+print(square(3))
+
+# by default all functions in Python return None
+# if you have a function that calculates something, you can return the result using return statement
