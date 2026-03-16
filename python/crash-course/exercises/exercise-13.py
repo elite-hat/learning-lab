e7d1a6fc396a8346890c2288525e43683fe9c535
elite-hat <elitehat.developer@gmail.com>
2026-03-16 21:23:02 +0500

    #

--- python/crash-course/exercises/exercise-13.py
+++ python/crash-course/exercises/exercise-13.py
@@ -1,1 +1,31 @@
 # creating a resuable function
+
+def emojiConvertor(message):
+    emojies = {
+        ":)": "🙂",
+        ":(": "🙁"
+    }
+    words = message.split(" ")
+    output = ""
+    for word in words:
+        output += emojies.get(word, word) + " "
+    return output
+
+message = input("> ")
+print(emojiConvertor(message))
+
+# Solution
+
+#   def emoji_convertor(message):
+#       words = message.split(" ")
+#       emojis = {
+#           ":)": "🙂",
+#           ":(": "🙁",
+#       }
+#       output = ""
+#       for word in words:
+#           output += emojis.get(word, word) + " "
+#       return output
+
+#   message = input("> ")
+#   print(emoji_convertor(message))
