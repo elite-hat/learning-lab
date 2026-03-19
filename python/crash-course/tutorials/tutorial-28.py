ed018eac0ade9faba3d0948e7cd0af71ebae0fc7
elite-hat <elitehat.developer@gmail.com>
2026-03-19 21:56:21 +0500

    #

--- python/crash-course/tutorials/tutorial-28.py
+++ python/crash-course/tutorials/tutorial-28.py
@@ -2,6 +2,10 @@
 
 try:
     age = int(input("Age: "))
+    income = 20000
+    risk = income / age
     print(age)
 except ValueError:
     print("Invalid Value")
+except ZeroDivisionError:
+    print("Age can not be Zero")
