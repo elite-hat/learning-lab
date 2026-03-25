ebbb5cfa704d88d5917d32d52588df424fad8752
elite-hat <elitehat.developer@gmail.com>
2026-03-25 21:04:51 +0500

    #

---
+++ python/projects/fizzbuzz/main.py
@@ -0,0 +1,16 @@
+title = 'fizz buzz'
+print(title.upper())
+print('Enter a range of numbers:')
+n1 = int(input('Start: '))
+n2 = int(input('End: '))
+i = n1
+while i <= n2:
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizz buzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
+    i += 1
