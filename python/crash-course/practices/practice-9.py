d96845cd3178be33938c7092a7905f13cdb65115
elite-hat <elitehat.developer@gmail.com>
2026-03-26 14:38:04 +0500

    #

--- python/crash-course/practices/practice-9.py
+++ python/crash-course/practices/practice-9.py
@@ -1,4 +1,10 @@
+import math
+
 x = -9.6
+print(x)
 
 print(round(x)) # to round a number to nearest integer
 print(abs(x)) # to convert into corresponding positive integer
+
+print(math.ceil(x)) # rounds to nearest small whole number
+print(math.floor(x)) # rounds to nearest large whole number
