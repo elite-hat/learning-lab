76782a656ba7adf766a95d0946864de0294fb613
elite-hat <elitehat.developer@gmail.com>
2026-03-26 17:51:15 +0500

    #

---
+++ python/crash-course/practices/practice-10.py
@@ -0,0 +1,11 @@
+day = True
+night = False
+if day:
+    print('It is a day.')
+    print('Focus on your work.')
+elif night:
+    print('It is a night.')
+    print('Just relax.')
+else:
+    print('It is either a morning or evening.')
+print('Whatever time it is, be positive, and progressive.')
