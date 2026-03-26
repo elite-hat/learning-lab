a29dc2760c3b5eed23ed0fca2f9502099662b0e1
elite-hat <elitehat.developer@gmail.com>
2026-03-26 18:01:14 +0500

    #

---
+++ python/crash-course/practices/practice-12.py
@@ -0,0 +1,7 @@
+marks = 0
+if marks >= 40 and marks <= 100:
+    print('Congratulations! You are passed.')
+elif marks < 40 and marks >= 0:
+    print('Sorry! You are failed.')
+else:
+    print('Invalid Input')
