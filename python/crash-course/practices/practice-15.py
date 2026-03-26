e01d2d09302fd4a617f69db345c0b05f82cd04a1
elite-hat <elitehat.developer@gmail.com>
2026-03-26 18:39:18 +0500

    #

--- python/crash-course/practices/practice-15.py
+++ python/crash-course/practices/practice-15.py
@@ -1,2 +1,12 @@
-for item in 'AbdunNafayRathore':
+for item in 'AbdunNafayRathore':                                # Characters
+    print(item)
+for item in ['person1', 'person2', 'person3', 'person4']:       # Strings
+    print(item)
+for item in [1, 2, 3, 4, 5]:                                    # Numbers
+    print(item)
+for item in range(10):                                          # From 0 to 10
+    print(item)
+for item in range(1, 5):                                        # From 1 to 4
+    print(item)
+for item in range(1, 10, 2):                                    # From 1 to 10 with the gap of 2
     print(item)
