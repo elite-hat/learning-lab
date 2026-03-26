11da77e6c2e53507d2d5c58b2cc9edb55d08fc84
elite-hat <elitehat.developer@gmail.com>
2026-03-26 17:57:23 +0500

    #

---
+++ python/crash-course/practices/practice-11.py
@@ -0,0 +1,10 @@
+a = 10
+b = 40
+c = 30
+
+if a < b and b < c:
+    print(f'{c} is the greatest number.')
+if b > c and c > a:
+    print(f'{b} is the greatest number.')
+if c < a and b < c:
+    print(f'{a} is the greatest number')
