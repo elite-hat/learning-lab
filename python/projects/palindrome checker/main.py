c3f0282490383e4e2cccd50e616e1961f4bf2a85
elite-hat <elitehat.developer@gmail.com>
2026-03-29 23:22:25 +0500

    #

--- python/projects/palindrome checker/main.py
+++ python/projects/palindrome checker/main.py
@@ -2,7 +2,7 @@ title = 'palindrome checker'
 print(title.upper())
 word = input('Enter a word: ')
 if word == word[::-1]:
-    print(f"Yes! {word} = {word[::-1]}")
+    print(f"Yes! {word} is equal to {word[::-1]}")
     print("So, it is a plaindrome.")
 else:
-    print('It is not a palindrome')
+    print(f"No! {word} is not equal to {word[::-1]}")
