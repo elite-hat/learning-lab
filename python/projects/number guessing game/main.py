47395e28e12bed6c1d32f8f6bf9c2220c5e55103
elite-hat <elitehat.developer@gmail.com>
2026-03-30 19:32:21 +0500

    #

--- python/projects/number guessing game/main.py
+++ python/projects/number guessing game/main.py
@@ -5,23 +5,26 @@ print(title.upper())
 print('Enter a range of numbers:')
 n1 = int(input('Starting number: '))
 n2 = int(input('Ending number: '))
-print('Computer is guessing a number...')
-computer = random.randint(n1, n2)
-time.sleep(1)
-print('Done, Computer has choosen a number.')
-print('Please, guess a number. (You have 3 lives)')
-lives = 3
-while lives > 0:
-    user = int(input('> '))
-    if user >= n1 and user <= n2:
-        if user == computer:
-            print(f'Congratulations!!! {user} is a correct number.')
-            break
+if n1 < n2:
+    print('Computer is guessing a number...')
+    computer = random.randint(n1, n2)
+    time.sleep(1)
+    print('Done, Computer has choosen a number.')
+    print('Please, guess a number. (You have 3 lives)')
+    lives = 3
+    while lives > 0:
+        user = int(input('> '))
+        if user >= n1 and user <= n2:
+            if user == computer:
+                print(f'Congratulations!!! {user} is a correct number.')
+                break
+            else:
+                lives -= 1
+                print(f'Sorry! {user} is not the correct number, you have {lives} live(s) remaining.')
         else:
-            lives -= 1
-            print(f'Sorry! {user} is not the correct number, you have {lives} live(s) remaining.')
+            print('Wrong Input')
     else:
-        print('Wrong Input')
+        print('Sorry, you have no lives left.')
+        print(f'The correct answer is {computer}.')
 else:
-    print('Sorry, you have no lives left.')
-    print(f'The correct answer is {computer}.')
+    print("Starting Number must be smaller than Ending Number")
