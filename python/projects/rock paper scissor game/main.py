126ffeebcbefe333d7ea6fd5d3a15778b61e0c37
elite-hat <elitehat.developer@gmail.com>
2026-03-31 20:42:54 +0500

    #

--- python/projects/rock paper scissor game/main.py
+++ python/projects/rock paper scissor game/main.py
@@ -11,30 +11,33 @@ moves = {
 }
 movesList = ["rock", "paper", "scissor"]
 
+wins = {
+    "rock": "scissor",
+    "paper": "rock",
+    "scissor": "paper"
+}
+
 print("\nMoves:")
 print("1. rock\n2. paper\n3. scissor")
 
 while True:
     print("\nComputer is choosing a move.")
     time.sleep(1)
-    computerGuess = random.randint(1, 3)
-    computerMove = moves[computerGuess]
+    computerChoice = random.randint(1, 3)
+    computerMove = moves[computerChoice]
 
-    print("\nNow, It's your turn to choose a move.")
-    userGuess = input("> ")
-    userMove = userGuess.lower()
+    print("Now, It's your turn to choose a move.")
+    userChoice = input("> ")
+    userMove = userChoice.lower()
 
     if userMove in movesList:
-        print(f"\nComputer's Move was {computerMove}")
-        if userMove == computerMove:
-            print("Draw! No live(s) deducted.")
-        elif (
-            (userMove == "rock" and computerMove == "scissor") or
-            (userMove == "paper" and computerMove == "rock") or
-            (userMove == "scissor" and computerMove == "paper")
-        ):
-            print("You Win! No live(s) deducted.")
-        else:
-            print("You lost! 1 life deducted.")
+            print(f"\nComputer's Choice: {computerMove}")
+            print(f"Your choice: {userMove}")
+            if userMove == computerMove:
+                print("Draw!")
+            elif wins[userMove] == computerMove:
+                print("You Win!")
+            else:
+                print("You lost!")
     else:
         print("Invalid Input")
