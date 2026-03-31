3aaf10c3cc1d527b67a0d4e2c94942dbdb52b4b1
elite-hat <elitehat.developer@gmail.com>
2026-03-31 16:05:55 +0500

    #

--- python/projects/fizzbuzz/main.py
+++ python/projects/fizzbuzz/main.py
@@ -1,18 +1,14 @@
-title = 'fizz buzz'
+title = "fizz buzz"
 print(title.upper())
-print('Enter a range of numbers:')
-n1 = int(input('Start: '))
-n2 = int(input('End: '))
-i = n1
-while i <= n2:
-    if i % 3 == 0 and i % 5 == 0:
-        print('fizz buzz')
-    elif i % 3 == 0:
-        print('fizz')
-    elif i % 5 == 0:
-        print('buzz')
-    else:
-        print(i)
+i = 0
+while i < 100:
     i += 1
-
-# FIXME: This program is not working correctly.
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    else:
+        if i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
