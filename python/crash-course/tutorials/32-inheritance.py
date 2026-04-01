73f0e71b8f4277e89cb9f869ce7b18cc462adaa7
elite-hat <elitehat.developer@gmail.com>
2026-04-01 12:26:37 +0500

    #

--- python/crash-course/tutorials/32-inheritance.py
+++ python/crash-course/tutorials/32-inheritance.py
@@ -8,9 +8,16 @@ class Mammal:
     def walk(self):
         print("walk")
 class Dog(Mammal):
-    pass
+    def bark(self):
+        print("bark")
 class Cat(Mammal):
-    pass
+    def be_annoying(self):
+        print("annoying")
 
 dog1 = Dog()
 dog1.walk()
+dog1.bark()
+
+cat1 = Cat()
+cat1.walk()
+cat1.be_annoying()
