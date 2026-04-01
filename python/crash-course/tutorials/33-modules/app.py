ddd33d85e5c8702e61ffa5b35ef38b0bc93e888b
elite-hat <elitehat.developer@gmail.com>
2026-04-01 12:53:30 +0500

    #

--- python/crash-course/tutorials/33-modules/app.py
+++ python/crash-course/tutorials/33-modules/app.py
@@ -1,3 +1,11 @@
 # MODULES
 
 # NOTE: Modules is to write code in multiple files, to organize our code.
+
+# Method 1
+import convertors
+print(convertors.kg_to_lbs(70))
+
+# Method 2
+from convertors import kg_to_lbs
+print(kg_to_lbs(70))
