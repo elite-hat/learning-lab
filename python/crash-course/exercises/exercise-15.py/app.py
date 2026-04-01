68201bab667b0045f90c1d42ca86f0b8163e2d55
elite-hat <elitehat.developer@gmail.com>
2026-04-01 13:21:59 +0500

    #

--- python/crash-course/exercises/exercise-15.py/app.py
+++ python/crash-course/exercises/exercise-15.py/app.py
@@ -2,3 +2,10 @@ import utils
 
 numbers = [10, 3, 6, 2]
 print(utils.find_max(numbers))
+
+#   Solution
+
+#   from utils import find_max
+#   numbers = [10, 3, 6, 2]
+#   max = find_max(numbers)
+#   print(max)
