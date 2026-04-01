21d1d676e21b0850999953af7ae45b8f910eaff4
elite-hat <elitehat.developer@gmail.com>
2026-04-01 16:09:41 +0500

    #

--- python/crash-course/tutorials/35-generatingRandomValues.py
+++ python/crash-course/tutorials/35-generatingRandomValues.py
@@ -4,3 +4,9 @@ import random
 
 for i in range(3):
     print(random.random())       # Picks a random float number between 0 and 1
+for i in range(3):
+    print(random.randint(10, 20))
+
+members = ["John", "Mary", "Bob", "Mosh"]
+leader = random.choice(members)
+print(leader)
