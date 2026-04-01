584222bbc2840a7fd7b209f19b27bf9c5eabd11d
elite-hat <elitehat.developer@gmail.com>
2026-04-01 09:49:28 +0500

    #

--- python/crash-course/exercises/exercise-99.py
+++ python/crash-course/exercises/exercise-99.py
@@ -7,3 +7,17 @@ class Person:
 person1 = Person("Abdun Nafay Rathore")
 
 person1.talk()
+
+# Solution
+
+#    class Person:
+#        def __init__(self, name):
+#            self.name = name
+#        def talk(self):
+#            print(f"Hi, I am {self.name}")
+
+#    john = Person("John Smith")
+#    john.talk()
+
+#    bob = Person("Bob Smith")
+#    bob.talk()
