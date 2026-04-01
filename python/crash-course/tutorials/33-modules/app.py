05ad41c7a12931101277ab84490508e27147e0ce
elite-hat <elitehat.developer@gmail.com>
2026-04-01 12:35:49 +0500

    #

--- python/crash-course/tutorials/33-modules/app.py
+++ python/crash-course/tutorials/33-modules/app.py
@@ -1,9 +1,3 @@
 # MODULES
 
 # NOTE: Modules is to write code in multiple files, to organize our code.
-
-def lbs_to_kg(weight):
-    return weight * 0.45
-
-def kg_to_lbs(weight):
-    return weight / 0.45
