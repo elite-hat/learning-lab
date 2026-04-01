4240c0260b8e69770ece7fb44d3eca20b405027e
elite-hat <elitehat.developer@gmail.com>
2026-04-01 22:15:19 +0500

    #

--- python/crash-course/projects/Excel Spreadsheets/app.py
+++ python/crash-course/projects/Excel Spreadsheets/app.py
@@ -1,3 +1,6 @@
 import openpyxl as xl
 
 wb = xl.load_workbook("transactions.xlsx")
+sheet = wb["Sheet1"]
+cell = sheet.cell(1, 1)         # It is equal to: `cell = sheet.cell(1,1)`
+print(cell.value)
