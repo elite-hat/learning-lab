ff25e08f1b2c8dace42281f7bc3a941dc7142e6c
elite-hat <elitehat.developer@gmail.com>
2026-04-01 09:48:06 +0500

    #

---
+++ python/crash-course/exercises/exercise-99.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def talk(self):
+        print(f"Hello I am {self.name}")
+
+person1 = Person("Abdun Nafay Rathore")
+
+person1.talk()
