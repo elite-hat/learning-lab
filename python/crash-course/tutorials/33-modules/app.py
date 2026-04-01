c75f0d8bf89712c7941ed8e777090c6305c467b2
elite-hat <elitehat.developer@gmail.com>
2026-04-01 12:57:05 +0500

    #

--- python/crash-course/tutorials/33-modules/app.py
+++ python/crash-course/tutorials/33-modules/app.py
@@ -9,3 +9,11 @@ print(convertors.kg_to_lbs(70))
 # Method 2
 from convertors import kg_to_lbs
 print(kg_to_lbs(70))
+
+# Summary
+'''
+1. We use modules to better organize our code
+2. We breakup our code in different files
+3. Each file is called a module, and it usually contains functions or classes
+4. There are two methods to import a module
+'''
