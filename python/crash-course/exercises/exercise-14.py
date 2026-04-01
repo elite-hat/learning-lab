e32e19ef9f4ab8eadd9810589fe2b0cae15f7f90
elite-hat <elitehat.developer@gmail.com>
2026-04-01 12:57:41 +0500

    #

--- python/crash-course/exercises/exercise-14.py
+++ python/crash-course/exercises/exercise-14.py
@@ -1,22 +1,23 @@
 class Person:
+    def __init__(self, name):
+        self.name = name
     def talk(self):
-        print('Hello')
+        print(f"Hello I am {self.name}")
 
-person1 = Person()
-person1.name = input('Uncle')
+person1 = Person("Abdun Nafay Rathore")
+
 person1.talk()
 
 # Solution
 
-class Person:
-    def __init__(self, name):
-        self.name = name
-    def talk(self):
-        print(f"Hi, I am {self.name}")
+#    class Person:
+#        def __init__(self, name):
+#            self.name = name
+#        def talk(self):
+#            print(f"Hi, I am {self.name}")
 
+#    john = Person("John Smith")
+#    john.talk()
 
-john = Person("John Smith")
-john.talk()
-
-bob = Person("Bob Smith")
-bob.talk()
+#    bob = Person("Bob Smith")
+#    bob.talk()
