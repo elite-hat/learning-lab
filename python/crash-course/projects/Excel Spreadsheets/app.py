0189dedb142fee52e545094cea25d9fa5fe237e3
elite-hat <elitehat.developer@gmail.com>
2026-04-02 09:20:02 +0500

    #

--- python/crash-course/projects/Excel Spreadsheets/app.py
+++ python/crash-course/projects/Excel Spreadsheets/app.py
@@ -2,5 +2,7 @@ import openpyxl as xl
 
 wb = xl.load_workbook("transactions.xlsx")
 sheet = wb["Sheet1"]
-cell = sheet.cell(1, 1)         # It is equal to: `cell = sheet.cell(1,1)`
+cell = sheet.cell(1, 1)         # It is equal to: `sheet["a1"]`
 print(cell.value)
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 1)
