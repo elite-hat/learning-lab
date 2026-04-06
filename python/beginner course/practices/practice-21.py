295e0ab626d4c95d322b2d04a35c3656e61e8dda
elite-hat <elitehat.developer@gmail.com>
2026-04-06 11:19:17 +0500

    #

---
+++ python/beginner course/practices/practice-21.py
@@ -0,0 +1,17 @@
+numbers = (1, 2, 3, 4, 5)
+
+a, b, c, d, e = numbers
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# alternate
+
+a = numbers[0]
+b = numbers[0]
+c = numbers[0]
+d = numbers[0]
+e = numbers[0]
