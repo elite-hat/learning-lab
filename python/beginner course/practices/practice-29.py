6cbe540ee280fbbb569023c249de5dd033152e46
elite-hat <elitehat.developer@gmail.com>
2026-04-06 13:13:13 +0500

    #

---
+++ python/beginner course/practices/practice-29.py
@@ -0,0 +1,12 @@
+# Comments are amazing to take notes, set reminders, label a statement all inside your code.
+
+# I use Better Comments extension in Vu=isual Studio Code.
+# That helps me to highlight import messages.
+
+# For Example:
+# NOTE: There is a note.
+# TODO: I have to do this.
+# ! This is an alert.
+# // This is a deleted comment.
+# ? This is a query.
+# * This is, I don't know, what it is.
