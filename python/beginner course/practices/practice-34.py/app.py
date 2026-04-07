14cc06e1a62e70a30d26c143221724073aa4236a
elite-hat <elitehat.developer@gmail.com>
2026-04-07 14:01:25 +0500

    #

---
+++ python/beginner course/practices/practice-34.py/app.py
@@ -0,0 +1,8 @@
+from introduction.main import personal_details as intro
+
+print("Enter your personal details.")
+name = input("Name: ")
+age = input("Age: ")
+gender = input("Gender: ")
+
+intro(name, age, gender)
--- python/beginner course/practices/practice-34.py/introduction/main.py
+++ python/beginner course/practices/practice-34.py/introduction/main.py
@@ -1,4 +1,4 @@
-def introduction(name, age, gender):
+def personal_details(name, age, gender):
     print("Hello, World!")
     print(f"I am {name}.")
     print(f"I am a {age} years old, {gender}.")
