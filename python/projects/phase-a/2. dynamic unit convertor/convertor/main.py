4b3450a3fb62e3cfc144a3b96e16c2f813c11b38
elite-hat <elitehat.developer@gmail.com>
2026-04-16 08:29:06 +0500

    #

--- python/projects/phase-a/2. dynamic unit convertor/convertor/main.py
+++ python/projects/phase-a/2. dynamic unit convertor/convertor/main.py
@@ -1,29 +1,35 @@
 categories = {
     "length" : {
-        "millimeters" : 1000,
-        "meters" : 1,
-        "kilometers" : 1 / 1000,
-        "inches" : 40,
-        "feet" : 10 / 3,
-        "yards" : 0.9144
+        "millimeters" : (1000, 0),
+        "meters" : (1, 0),
+        "kilometers" : (1 / 1000, 0),
+        "inches" : (40, 0),
+        "feet" : (10 / 3, 0),
+        "yards" : (0.9144, 0)
     },
 
     "mass" : {
-        "milligrams" : 1000000,
-        "grams" : 1000,
-        "kilograms" : 1,
-        "tonnes" : 1 / 1000,
-        "pounds" : 2.205
+        "milligrams" : (1000000, 0),
+        "grams" : (1000, 0),
+        "kilograms" : (1, 0),
+        "tonnes" : (1 / 1000, 0),
+        "pounds" : (2.205, 0)
     },
 
     "time" : {
-        "seconds" : 1,
-        "minutes" : 1 / 60,
-        "hours" : 1 / 3600,
-        "days" : 1 / 86400,
-        "months" : 1 / 2592000,
-        "years" : 1 / 31536000,
-        "century" : 1 / 3153600000
+        "seconds" : (1, 0),
+        "minutes" : (1 / 60, 0),
+        "hours" : (1 / 3600, 0),
+        "days" : (1 / 86400, 0),
+        "months" : (1 / 2592000, 0),
+        "years" : (1 / 31536000, 0),
+        "century" : (1 / 3153600000, 0)
+    },
+
+    "temperature" : {
+        "celsius" : (1, 0),
+        "fahrenheit" : (5/9, 32),
+        "kelvin" : (1, -273.15)
     }
 }
 
@@ -42,11 +48,12 @@ def unit_options(category):
 
 def process(category, from_option, to_option):
     categ = categories[category]
-    global unit
-    unit = categ[from_option]
-    global convert
-    convert = categ[to_option]
+    global unit_factor, unit_offset
+    unit_factor, unit_offset = categ[from_option]
+    global convert_factor, convert_offset
+    convert_factor, convert_offset = categ[to_option]
 
 def display(value):
-    result = (value / unit) * convert
+    base_value = (value - unit_offset) / unit_factor
+    result = base_value * convert_factor + convert_offset
     return (f"\nResult: {result}")
